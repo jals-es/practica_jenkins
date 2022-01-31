f82fd92e1477cf25599757a9923ed31f71ca14e3
Juan Antonio Lopez Segui <juan.antonio.lis@gmail.com>
2022-02-01 00:05:43 +0100

    update readme

--- jenkinsScripts/update_readme.js
+++ jenkinsScripts/update_readme.js
@@ -11,7 +11,7 @@ fs.readFile('README.md', 'utf-8', (err, data) => {
     }
     const markdownUpdate = data.replace(
         /(?<=\[!\[Cypress.io\]\()[\s\S]*(?=\)\])/gm,
-        testResult !== 'failure' ? successBadge : failedBadge
+        testResult == 0 ? successBadge : failedBadge
     )
     fs.writeFile('README.md', markdownUpdate, 'utf-8', (err) => {
         if (err) {
