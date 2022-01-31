c22deb10164a739e015c615325b3d6b6137af4b3
Juan Antonio Lopez Segui <juan.antonio.lis@gmail.com>
2022-01-31 22:14:32 +0100

    updateReadme job

---
+++ jenkinsScripts/update_readme.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+
+const testResult = process.argv[2]
+console.log(testResult);
+const successBadge = 'https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg'
+const failedBadge = 'https://img.shields.io/badge/test-failure-red.svg'
+
+fs.readFile('README.md', 'utf-8', (err, data) => {
+    if (err) {
+        throw err
+    }
+    const markdownUpdate = data.replace(
+        /(?<=\[!\[Cypress.io\]\()[\s\S]*(?=\)\])/gm,
+        testResult !== 'failure' ? successBadge : failedBadge
+    )
+    fs.writeFile('README.md', markdownUpdate, 'utf-8', (err) => {
+        if (err) {
+            throw err
+        }
+        console.log('Markdown Update Successfully')
+    })
+})
